0d4400ca47207a6f23f2462a0761e06c38e7884d
komugi333 <onipy2525@icloud.com>
2025-06-01 18:13:42 +0900

    prototype

---
+++ kazuate1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf("Guess the numbers 0 to 9.\n");
+
+	int ans = 7;
+	int no;
+
+	printf("What is the number?: ");
+	scanf("%d", &no);
+
+	if (no > ans)
+		printf("Too large.\a\n");
+	else if (no < ans)
+		printf("Too small.\a\n");
+	else
+		printf("Correct answer!\n");
+
+}
